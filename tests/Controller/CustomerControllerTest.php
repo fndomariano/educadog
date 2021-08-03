a83bd6965d42c622c02d826677bafe2d548bce7c
Fernando Mariano <fernando.mariano@totvs.com.br>
2021-08-03 20:55:14 -0300

    wip: customer controller

--- tests/Controller/CustomerControllerTest.php
+++ tests/Controller/CustomerControllerTest.php
@@ -4,31 +4,48 @@ namespace Tests\Controller;
 
 use App\Models\User;
 use App\Models\Customer;
+use App\Http\Requests\CustomerRequest;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Http\UploadedFile;
 use Tests\TestCase;
 
 class CustomerControllerTest extends TestCase {
 
     use RefreshDatabase;
 
-    public function testOnlyAuthenticatedUsersCanSeeCustomers() {
+    private $rulesMessages;
+
+    public function setUp() : void {
+        parent::setUp();
+        $this->rulesMessages = (new CustomerRequest)->messages();
+    }
+
+    /**
+     * Deve redirecionar para página de login quando não está autenticado.
+     */
+    public function testOnlyAuthenticatedUsersCanSeeCustomers() : void {
         $this->get('/customers')             
              ->assertRedirect('/login');
     }
 
-    public function testListCustomers() {
+    /**
+     * Deve listar clientes
+     */
+    public function testListCustomers() : void {
         
         $this->actingAs(User::factory()->create());
         
         $this->get('/customers')->assertOk();
     }
 
-    public function testCreateCustomer() {
+    /**
+     * Deve salvar um novo cliente
+     */
+    public function testStoreCustomer() : void {
 
         $data = [
             'name'     => 'Fernando',
             'email'    => 'fernando.mariano@gmail.com',
-            'active'   => true,
             'phone'    => '47989940098'
         ];
 
@@ -38,5 +55,129 @@ class CustomerControllerTest extends TestCase {
             ->assertRedirect('/customers');
 
         $this->assertDatabaseHas('customer', $data);
+    }
+
+    /**
+     * Deve salvar as alterações de um cliente
+     */
+    public function testUpdateCustomer() : void {
+
+        $customer = Customer::factory()->create();
+
+        $data = [
+            'name'     => 'Caroline Dirschnabel',
+            'email'    => 'carol.dirsch@gmail.com',
+            'active'   => true,
+            'phone'    => '47986292309'
+        ];
+
+        $this
+            ->actingAs(User::factory()->create())
+            ->put(sprintf('/customer/%s/update', $customer->id), $data)
+            ->assertRedirect('/customers');
+
+        $this->assertDatabaseHas('customer', $data);
+    }
+
+    /**
+     * Deve excluir um cliente
+     */
+    public function testDestroyCustomer() : void {
+        
+        $customer = Customer::factory()->create();
+
+        $this
+            ->actingAs(User::factory()->create())
+            ->delete(sprintf('/customer/%s/destroy', $customer->id))
+            ->assertRedirect('/customers');
+
+        $this->assertDatabaseMissing('customer', ['id' => $customer->id]);
+    }
+
+    /**
+     * Deve efetuar a validação de campos obrigatórios do cliente
+     */
+    public function testCustomerRequiredFields() : void {
+        
+        $response = $this
+            ->actingAs(User::factory()->create())
+            ->post('/customer/store', []);
+
+        $response->assertSessionHasErrors([
+            'name' => $this->rulesMessages['name.required'], 
+            'email' => $this->rulesMessages['email.required'], 
+            'phone' => $this->rulesMessages['phone.required']
+        ]);
+    }
+
+    /**
+     * Deve efetuar a validação de e-mail único
+     */
+    public function testCustomerUniqueEmail() : void {
+
+        $customer = Customer::factory()->create();
+
+        $data = [
+            'name'     => 'Caroline Dirschnabel',
+            'email'    => $customer->email,
+            'active'   => true,
+            'phone'    => '47986292309'
+        ];
+
+        $response = $this
+            ->actingAs(User::factory()->create())
+            ->post('/customer/store', $data);
+            
+        $response->assertSessionHasErrors([
+            'email' => $this->rulesMessages['email.unique']
+        ]);
+    }
+
+    /**
+     * Deve validar extensão do arquivo de contrato
+     */
+    public function testCustomerContractExtension() : void {
+        
+        $file = UploadedFile::fake()->create('tests/test.txt');
+
+        $data = [
+            'name'     => 'Caroline Dirschnabel',
+            'email'    => 'carol.dirsch@gmail.com',
+            'active'   => true,
+            'phone'    => '47986292309',
+            'contract' => $file
+        ];
+
+        $response = $this
+            ->actingAs(User::factory()->create())
+            ->post('/customer/store', $data);
+            
+        $response->assertSessionHasErrors([
+            'contract' => $this->rulesMessages['contract.mimes']
+        ]);
+    }
+
+    /**
+     * Deve validar tamanho do arquivo de contrato
+     */
+    public function testCustomerContractSize() : void {
+        
+        $file = UploadedFile::fake()->create('tests/test.pdf', 6000);
+
+        $data = [
+            'name'     => 'Caroline Dirschnabel',
+            'email'    => 'carol.dirsch@gmail.com',
+            'active'   => true,
+            'phone'    => '47986292309',
+            'contract' => $file
+        ];
+
+        $response = $this
+            ->actingAs(User::factory()->create())
+            ->post('/customer/store', $data);
+            
+        $response->assertSessionHasErrors([
+            'contract' => $this->rulesMessages['contract.max']
+        ]);
     }
 }
